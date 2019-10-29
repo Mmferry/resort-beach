3d5b0d1ba6681d53a6be74472ed931bb8b233f5b
mferry <mfaired@gmail.com>
2019-10-29 23:31:24 +0200

    Updates

---
+++ precache-manifest.495da8c41e0ec3d35d588d575dda842d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7beff2a7bcad7af678672da5d90ecb1a",
+    "url": "/resort/index.html"
+  },
+  {
+    "revision": "61da1dfe3ee8041aadf5",
+    "url": "/resort/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "361d16423a038ce8d31d",
+    "url": "/resort/static/js/2.67821b20.chunk.js"
+  },
+  {
+    "revision": "61da1dfe3ee8041aadf5",
+    "url": "/resort/static/js/main.d3e292e4.chunk.js"
+  },
+  {
+    "revision": "9ee43823a567943c4351",
+    "url": "/resort/static/js/runtime-main.19dcc410.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/resort/static/media/logo.25bf045c.svg"
+  }
+]);
